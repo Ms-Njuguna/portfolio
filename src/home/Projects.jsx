dcb63f904638c7900e505fb92b85f77f8cfc4dfd
Patricia <patricianjuguna084@gmail.com>
2026-02-16 22:33:55 +0300

    feat: add scroll up and down text reveal effect

---
+++ src/home/Projects.jsx
@@ -0,0 +1,33 @@
+import ScrollFloat from "../ui/ScrollFloat"
+
+export default function Projects() {
+    return (
+        <section className="min-h-7xl">
+            <ScrollFloat animationDuration={1} ease='back.inOut(2)' scrollStart='center bottom+=50%' scrollEnd='bottom bottom-=40%' stagger={0.03}>
+                Projects
+            </ScrollFloat>
+            <p>guscgushiohdiaidhsdxiohsuvfshiohoihsoeihoifhrshiohuerxdilhgizrsufiusriuhfusehfhilshziuhzfuhsuiehfihzsihfuzes</p>
+            <p>guscgushiohdiaidhsdxiohsuvfshiohoihsoeihoifhrshiohuerxdilhgizrsufiusriuhfusehfhilshziuhzfuhsuiehfihzsihfuzes</p>
+            <p>guscgushiohdiaidhsdxiohsuvfshiohoihsoeihoifhrshiohuerxdilhgizrsufiusriuhfusehfhilshziuhzfuhsuiehfihzsihfuzes</p>
+            <p>guscgushiohdiaidhsdxiohsuvfshiohoihsoeihoifhrshiohuerxdilhgizrsufiusriuhfusehfhilshziuhzfuhsuiehfihzsihfuzes</p>
+            <p>guscgushiohdiaidhsdxiohsuvfshiohoihsoeihoifhrshiohuerxdilhgizrsufiusriuhfusehfhilshziuhzfuhsuiehfihzsihfuzes</p>
+            <p>guscgushiohdiaidhsdxiohsuvfshiohoihsoeihoifhrshiohuerxdilhgizrsufiusriuhfusehfhilshziuhzfuhsuiehfihzsihfuzes</p>
+            <p>guscgushiohdiaidhsdxiohsuvfshiohoihsoeihoifhrshiohuerxdilhgizrsufiusriuhfusehfhilshziuhzfuhsuiehfihzsihfuzes</p>
+            <p>guscgushiohdiaidhsdxiohsuvfshiohoihsoeihoifhrshiohuerxdilhgizrsufiusriuhfusehfhilshziuhzfuhsuiehfihzsihfuzes</p>
+            <p>guscgushiohdiaidhsdxiohsuvfshiohoihsoeihoifhrshiohuerxdilhgizrsufiusriuhfusehfhilshziuhzfuhsuiehfihzsihfuzes</p>
+            <p>guscgushiohdiaidhsdxiohsuvfshiohoihsoeihoifhrshiohuerxdilhgizrsufiusriuhfusehfhilshziuhzfuhsuiehfihzsihfuzes</p>
+            <p>guscgushiohdiaidhsdxiohsuvfshiohoihsoeihoifhrshiohuerxdilhgizrsufiusriuhfusehfhilshziuhzfuhsuiehfihzsihfuzes</p>
+            <p>guscgushiohdiaidhsdxiohsuvfshiohoihsoeihoifhrshiohuerxdilhgizrsufiusriuhfusehfhilshziuhzfuhsuiehfihzsihfuzes</p>
+            <p>guscgushiohdiaidhsdxiohsuvfshiohoihsoeihoifhrshiohuerxdilhgizrsufiusriuhfusehfhilshziuhzfuhsuiehfihzsihfuzes</p>
+            <p>guscgushiohdiaidhsdxiohsuvfshiohoihsoeihoifhrshiohuerxdilhgizrsufiusriuhfusehfhilshziuhzfuhsuiehfihzsihfuzes</p>
+            <p>guscgushiohdiaidhsdxiohsuvfshiohoihsoeihoifhrshiohuerxdilhgizrsufiusriuhfusehfhilshziuhzfuhsuiehfihzsihfuzes</p>
+            <p>guscgushiohdiaidhsdxiohsuvfshiohoihsoeihoifhrshiohuerxdilhgizrsufiusriuhfusehfhilshziuhzfuhsuiehfihzsihfuzes</p>
+            <p>guscgushiohdiaidhsdxiohsuvfshiohoihsoeihoifhrshiohuerxdilhgizrsufiusriuhfusehfhilshziuhzfuhsuiehfihzsihfuzes</p>
+            <p>guscgushiohdiaidhsdxiohsuvfshiohoihsoeihoifhrshiohuerxdilhgizrsufiusriuhfusehfhilshziuhzfuhsuiehfihzsihfuzes</p>
+            <p>guscgushiohdiaidhsdxiohsuvfshiohoihsoeihoifhrshiohuerxdilhgizrsufiusriuhfusehfhilshziuhzfuhsuiehfihzsihfuzes</p>
+            <p>guscgushiohdiaidhsdxiohsuvfshiohoihsoeihoifhrshiohuerxdilhgizrsufiusriuhfusehfhilshziuhzfuhsuiehfihzsihfuzes</p>
+            <p>guscgushiohdiaidhsdxiohsuvfshiohoihsoeihoifhrshiohuerxdilhgizrsufiusriuhfusehfhilshziuhzfuhsuiehfihzsihfuzes</p>
+            <p>guscgushiohdiaidhsdxiohsuvfshiohoihsoeihoifhrshiohuerxdilhgizrsufiusriuhfusehfhilshziuhzfuhsuiehfihzsihfuzes</p>
+        </section>
+    )
+}
